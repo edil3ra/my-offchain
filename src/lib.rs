6e167e2a92c882e0cb4c8e4cc0e151a658cfac8e
edil3ra <edil3ra@gmail.com>
2022-08-20 13:33:29 +0200

    :construction: group transactions by client

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(slice_group_by)]
+
 use serde::Deserialize;
 use std::{env, error::Error, fs::File};
 
@@ -6,21 +8,31 @@ struct Transaction {
     #[serde(rename = "type")]
     transaction_type: String,
     client: u16,
-    txt: u32,
+    tx: u32,
     amount: Option<f32>,
 }
+
 
 pub fn run() -> Result<(), Box<dyn Error>> {
     let filename = env::args().nth(1).ok_or("missing filename")?;
     let file = File::open(&filename).map_err(|e| e.to_string())?;
     let mut csv_data = csv::Reader::from_reader(&file);
-    let transactions = csv_data
+    let mut transactions = csv_data
         .deserialize()
         .into_iter()
         .collect::<Result<Vec<Transaction>, csv::Error>>()?;
 
+
+    transactions.sort_by_key(|transaction| transaction.client);
+    let transactions_group_by_client = transactions
+        .group_by(|a, b| a.client == b.client)
+        .map(|group| group.to_vec())
+        .collect::<Vec<Vec<Transaction>>>();
+
+
     dbg!(&file);
     dbg!(&csv_data);
     dbg!(&transactions);
+    dbg!(&transactions_group_by_client);
     Ok(())
 }
