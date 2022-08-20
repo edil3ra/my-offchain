8724c68bdc046e2d812c50525401f1c3b8460da1
edil3ra <edil3ra@gmail.com>
2022-08-20 16:05:05 +0200

    :test_tube: add for failing  to withdraw

--- tests/cli.rs
+++ tests/cli.rs
@@ -2,21 +2,32 @@ use assert_cmd::Command;
 use predicates::prelude::*;
 use std::fs;
 
+
+const PRG: &str = "my-offchain";
+
 type TestResult = Result<(), Box<dyn std::error::Error>>;
 
-struct Test {
+struct TestSuccess {
     input: &'static str,
     out: &'static str,
 }
-const PRG: &str = "my-offchain";
 
-const WITHDRAWAL: Test = Test {
-    input: "tests/inputs/in_withdrawal.csv",
-    out: "tests/expected/out_withdrawal.csv",
+struct TestFailure {
+    input: &'static str,
+}
+
+const WITHDRAWAL_SUCESS: TestSuccess = TestSuccess {
+    input: "tests/inputs/in_withdrawal_success.csv",
+    out: "tests/expected/out_withdrawal_success.csv",
 };
 
 
-fn run(test: &Test) -> TestResult {
+const WITHDRAWAL_FAILURE: TestFailure = TestFailure {
+    input: "tests/inputs/in_withdrawal_failure.csv",
+};
+
+
+fn run(test: &TestSuccess) -> TestResult {
     let input = fs::read_to_string(test.input)?;
     let expected = fs::read_to_string(test.out)?;
 
@@ -28,8 +39,25 @@ fn run(test: &Test) -> TestResult {
     Ok(())
 }
 
+fn run_fail(test: &TestFailure, message: &str) -> TestResult {
+    let input = fs::read_to_string(test.input)?;
+
+    Command::cargo_bin(PRG)?
+        .arg(test.input)
+        .assert()
+        .success()
+        .stderr(predicate::str::contains(message));
+    Ok(())
+}
+
 
 #[test]
 fn withdrawal_sucess() -> TestResult {
-    run(&WITHDRAWAL)
+    run(&WITHDRAWAL_SUCESS)
+}
+
+
+#[test]
+fn withdrawal_should_fail_when_cannot_withdrawal() -> TestResult {
+    run_fail(&WITHDRAWAL_FAILURE, "error")
 }
